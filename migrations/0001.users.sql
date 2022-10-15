dc21fa0d97ca9839108f2bb48838c87130a3f0ef
Vitalii Poterba <vptrbv@gmail.com>
2022-10-15 19:24:26 +0300

    users migration fixed

--- migrations/0001.users.sql
+++ migrations/0001.users.sql
@@ -2,9 +2,9 @@
 
 CREATE TABLE roles (id SERIAL PRIMARY KEY, name TEXT);
 
-CREATE TABLE user_entries (id SERIAL PRIMARY KEY, tg_id TEXT, viber_id TEXT);
+CREATE TABLE users (id SERIAL PRIMARY KEY, tg_id TEXT, viber_id TEXT);
 
-CREATE TABLE civils (id SERIAL PRIMARY KEY, district_id INT, address TEXT, contact TEXT, coords_id INT);
-ALTER TABLE civils ADD FOREIGN KEY(district_id) REFERENCES districts(id);
-ALTER TABLE civils ADD FOREIGN KEY(coords_id) REFERENCES coords(id);
-
+CREATE TABLE promotions (id SERIAL PRIMARY KEY, users_id INT, role_id INT, date TIMESTAMP, promoter INT);
+ALTER TABLE promotions ADD FOREIGN KEY(users_id) REFERENCES users(id);
+ALTER TABLE promotions ADD FOREIGN KEY(role_id) REFERENCES roles(id);
+ALTER TABLE promotions ADD FOREIGN KEY(promoter) REFERENCES users(id);
