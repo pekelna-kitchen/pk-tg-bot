de9126e289f33e1833ef225f3d71d06ef325a6a8
Vitalii Poterba <vptrbv@gmail.com>
2022-10-23 16:43:30 +0300

    omit host specification

--- piku/deploy.sh
+++ piku/deploy.sh
@@ -4,7 +4,6 @@ set +x
 set -e
 
 PIKU_APP_NAME=$1
-HOST_NAME=$2
 
 # check if empty
 
@@ -14,8 +13,8 @@ if [ "$branch_name" != "master" ]; then
     app_name=${PIKU_APP_NAME}-test
 fi
 
-piku_address="piku@${HOST_NAME}:${app_name}"
-echo ${USER} updating piku: ${piku_address}
+piku_address="piku@localhost:${app_name}"
+echo ${USER} updating piku remote: ${piku_address}
 git remote add piku ${piku_address} || git remote set-url piku ${piku_address}
 
 piku deploy
